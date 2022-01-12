4b63718bb25bce671669c7c9176d649c8f93ffbb
Marta M Aguilera <73697174+martamagui@users.noreply.github.com>
2022-01-12 11:13:54 +0000

    Save function working

--- UD3-02-FileManagerYTablas-MartaMolina/ViewController.swift
+++ UD3-02-FileManagerYTablas-MartaMolina/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txtApellidos: UITextField!
+    @IBOutlet weak var txtNombre: UITextField!
+    var listaPersonas = [[String:String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func guardar(_ sender: Any) {
+        let nombre: String = txtNombre.text ?? ""
+        let apellidos : String = txtApellidos.text ?? ""
+        if(!(nombre == "") && !(apellidos == ""))
+        {
+            var persona : [String:String] = ["nombre": nombre,"apellidos":apellidos]
+            listaPersonas.append(persona)
+            txtNombre.text = ""
+            txtApellidos.text = ""
+        }
+    }
 }
 
