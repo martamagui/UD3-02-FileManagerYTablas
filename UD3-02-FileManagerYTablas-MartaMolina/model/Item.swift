92231bf4189f2318bd31b9fb6c94a5eb389cb840
Marta M Aguilera <73697174+martamagui@users.noreply.github.com>
2022-01-19 13:31:51 +0000

    Created item struct

---
+++ UD3-02-FileManagerYTablas-MartaMolina/model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  UD3-02-FileManagerYTablas-MartaMolina
+//
+//  Created by user211717 on 1/19/22.
+//
+
+import Foundation
+
+struct Item {
+    var nombre: String
+    var apellidos: String
+}
