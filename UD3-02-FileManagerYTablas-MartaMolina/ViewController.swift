2ff3b7f9dbf18abad40f2d0dca0b8e9bad4a631f
Marta M Aguilera <73697174+martamagui@users.noreply.github.com>
2022-01-31 18:47:44 +0000

    Empezando con los cambios en contrains

--- UD3-02-FileManagerYTablas-MartaMolina/ViewController.swift
+++ UD3-02-FileManagerYTablas-MartaMolina/ViewController.swift
@@ -13,14 +13,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var tvFeedBack: UILabel!
     @IBOutlet weak var txtNickUsuario: UITextField!
     
+    //Constrainst
+    @IBOutlet weak var altoLogo: NSLayoutConstraint!
+    @IBOutlet weak var anchoLogo: NSLayoutConstraint!
+    @IBOutlet weak var nickUsuariodrch: NSLayoutConstraint!
+    @IBOutlet weak var nickUsuarioizq: NSLayoutConstraint!
+    @IBOutlet weak var nickAlblNombre: NSLayoutConstraint!
+    @IBOutlet weak var nombreIzq: NSLayoutConstraint!
+    @IBOutlet weak var nombreDrch: NSLayoutConstraint!
+    @IBOutlet weak var altoNick: NSLayoutConstraint!
+    @IBOutlet weak var altoNombre: NSLayoutConstraint!
+    @IBOutlet weak var altoApellidos: NSLayoutConstraint!
+    @IBOutlet weak var apellidosDrch: NSLayoutConstraint!
+    @IBOutlet weak var apellidosIzq: NSLayoutConstraint!
+    @IBOutlet weak var altoFeedBack: NSLayoutConstraint!
+    @IBOutlet weak var separacionBotBtn: NSLayoutConstraint!
+    
+    
+    
+    
+    
     var xmlUsuario = String()
     var listaPersonas = [[String:String]]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    //Vista
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        let orientation = UIDevice.current.orientation
+        if orientation.isLandscape
+        {
+            altoLogo.constant = 30
+            anchoLogo.constant = 30
+        }
+        else
+        {
+            altoLogo.constant = 100
+            anchoLogo.constant = 100
+        }
+    }
     
+    //Logica
     @IBAction func guardar(_ sender: Any) {
         let usuario : Bool = recogerDatos()
         if(usuario){
